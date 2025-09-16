9d23d0ff545354b497f5b328a7a7097eb0048af7
Mark04022003 <ITITIU21187@student.hcmiu.edu.vn>
2025-09-16 15:16:10 +0700

    First Upload
    - Basic control (keyboard and mouse inputs)
    - Level (only 1 level)
    - Characters (animations are usually updated for better quality and match the game's functions)
    - Terrain (will be changed in the future)

---
+++ src/entities/Player.java
@@ -0,0 +1,211 @@
+package entities;
+
+import static utils.Constants.PlayerConstants.*;
+import static utils.Constants.PlayerConstants.GetSpriteAmount;
+
+import static utils.HelpMethods.*;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import utils.LoadSave;
+
+public class Player extends Entity{
+
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 20;
+	private int playerAction = IDLE;
+	private int playerDir = -1;
+	private boolean moving = false, attacking = false;
+	private boolean left, up, right, down;
+	private float playerSpeed = 2.0f;
+	private int[][]	 lvlData;
+	private float xDrawOffset = 12 * main.Game.SCALE;
+	private float yDrawOffset = 16 * main.Game.SCALE;
+	
+	// Jumping / Gravity 
+	private float airSpeed = 0f;
+	private float gravity = 0.04f * main.Game.SCALE;
+	private float jumpSpeed = -2.25f * main.Game.SCALE;
+	private float fallSpeedAfterCollision = 0.5f * main.Game.SCALE;
+	private boolean inAir = false;
+
+    
+	
+	public Player(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		loadAnimations();
+//		initHitbox((int) (x + xDrawOffset), (int) (y + yDrawOffset), (int) (width - 2 * xDrawOffset), (int) (height - yDrawOffset));
+	    initHitbox(x, y, 23 * main.Game.SCALE, 38 * main.Game.SCALE);
+	}
+	
+	public void update() {
+		
+		updatePos();
+		updateAnimationTick();
+		setAnimation();
+
+	}
+	
+	public void render(Graphics g) {
+	    g.drawImage(animations[playerAction][aniIndex],(int) (hitbox.x - xDrawOffset),(int) (hitbox.y - yDrawOffset), width, height, null);
+	    drawHitbox(g);
+	}
+	
+	
+	
+	private void updateAnimationTick() {
+			
+			aniTick++;
+			if(aniTick >= aniSpeed) {
+				aniTick = 0;
+				aniIndex++;
+				if(aniIndex >= GetSpriteAmount(playerAction)) {
+					aniIndex = 0;
+					attacking = false;
+				}
+			}
+			
+		}
+
+	private void setAnimation() {
+		
+		int startAni = playerAction;
+		
+		if (moving) {
+				
+				playerAction = RUNNING;
+			}
+		else 
+			
+			playerAction = IDLE;
+			
+		
+		if (attacking) {
+				playerAction = ATTACK_1;
+			}
+		
+		if (startAni != playerAction) {
+			resetAniTick();
+			}
+		}
+
+	
+	private void resetAniTick() {
+		aniTick = 0;
+		aniIndex = 0;
+	}
+
+	private void updatePos() {
+		
+		moving = false;
+		
+		if (!left && !right && !inAir)
+			return;
+		
+		float xSpeed = 0;
+		
+		if (left) {
+			xSpeed -= playerSpeed;
+		}
+		
+		if (right) {
+			xSpeed += playerSpeed;
+		}
+		
+		if (inAir) {
+
+		}
+		else {
+			updateXPos(xSpeed);
+		}
+		
+//		if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+//			hitbox.x += xSpeed;
+//			hitbox.y += ySpeed;
+//			moving = true;
+//		}
+		
+	}
+	private void updateXPos(float xSpeed) {
+		if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
+		hitbox.x += xSpeed;
+		} else {
+			hitbox.x = GetEntityXPosNextToWall(hitbox, xSpeed);
+			}
+		
+	}
+
+	private void loadAnimations() {
+		
+		
+			BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+			
+			animations = new BufferedImage[9][9];
+			for (int j = 0; j < animations.length; j++) {
+				for (int i = 0; i < animations[j].length; i++) { // FIX: use animations[j].length
+					animations[j][i] = img.getSubimage(i*32, j*32, 32, 32);
+				}
+			}
+			
+	}
+	
+	public void loadLvlData(int[][] lvlData) {
+		this.lvlData = lvlData;
+		// Debug: print the tile value at the player's starting position
+		int tileX = (int)(x / main.Game.TILES_SIZE);
+		int tileY = (int)(y / main.Game.TILES_SIZE);
+		int tileValue = lvlData[tileY][tileX];
+		System.out.println("Player start tile value: " + tileValue + " at (" + tileX + ", " + tileY + ")");
+		}
+	
+	
+	public void resetDirBooleans() {
+		left = false;
+		up = false;
+		right = false;
+		down = false;
+	}
+	
+	public void setAttacking(boolean attacking) {
+		this.attacking = attacking;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	
+}
---
+++ src/utils/Constants.java
@@ -0,0 +1,49 @@
+package utils;
+
+public class Constants {
+	
+	public static class Directions{
+		public static final int LEFT = 0;
+		public static final int UP = 1;
+		public static final int RIGHT = 2;
+		public static final int DOWN = 3;
+	}
+
+	public static class PlayerConstants{
+		public static final int IDLE = 0;
+		public static final int RUNNING = 1;
+		public static final int ATTACK_1 = 2;
+		public static final int JUMP = 3;
+		public static final int FALL = 4;
+		public static final int LANDING = 5;
+		public static final int ATTACK_2 = 6;
+		public static final int HURT =7;
+		public static final int DEATH = 8;
+		
+		public static final int GetSpriteAmount(int player_action) {
+			switch(player_action) {
+			case IDLE:
+				return 6;
+			case RUNNING:
+				return 8;
+			case ATTACK_1:
+				return 6;
+			case JUMP:
+				return 3;
+			case FALL:
+				return 1;
+			case LANDING:
+				return 2;
+			case ATTACK_2:
+				return 9;
+			case HURT:
+				return 4;
+			case DEATH:
+				return 4;
+			default:
+				return 1;
+			}
+		}
+	}
+	
+}
