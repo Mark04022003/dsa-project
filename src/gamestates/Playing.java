596409e87ac0fa1c27cce9b7a4dff4f696b4b7f5
Mark04022003 <ITITIU21187@student.hcmiu.edu.vn>
2025-09-19 17:53:30 +0700

    update n+1
    - Added larger level
    - Added background (+ moving fancy clouds)
    - Fixed A-D still having moving animations
    - Fixed various...

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -1,13 +1,18 @@
 package gamestates;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
+import static utils.Constants.Environment.*;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
 import ui.PauseOverlay;
+import utils.LoadSave;
 
 public class Playing extends State implements Statemethods{
 	
@@ -16,9 +21,27 @@ public class Playing extends State implements Statemethods{
 	private PauseOverlay pauseOverlay;
 	private boolean paused = false;
 	
+	private int xLvlOffset;
+	private int leftBorder = (int) (0.2 * Game.GAME_WIDTH);
+	private int rightBorder = (int) (0.8 * Game.GAME_WIDTH);
+	private int lvlTilesWide = LoadSave.GetLevelData()[0].length;
+	private int maxTileOffsetX = lvlTilesWide - Game.TILES_IN_WIDTH;
+	private int maxLvlOffsetX = maxTileOffsetX * Game.TILES_SIZE;
+	
+	private BufferedImage backgroundPlayingImg, bigCloud, smallCloud;
+	private int[] smallCloudPos;
+	private Random rnd = new Random();
+	
 	public Playing(Game game) {
 		super(game);
 		initClasses();
+		
+		backgroundPlayingImg = LoadSave.GetSpriteAtlas(LoadSave.PLAYING_BG_IMG);
+		bigCloud = LoadSave.GetSpriteAtlas(LoadSave.BIG_CLOUDS);
+		smallCloud = LoadSave.GetSpriteAtlas(LoadSave.SMALL_CLOUDS);
+		smallCloudPos = new int[8];
+		for(int i = 0; i < smallCloudPos.length; i++)
+			smallCloudPos[i] = (int) (70 * Game.SCALE) + rnd.nextInt((int) (150 * Game.SCALE));
 	}
 
 
@@ -37,6 +60,7 @@ public class Playing extends State implements Statemethods{
 		if(!paused) {
 			player.update();
 			levelManager.update();
+			checkCloseToBorder();
 		}
 		else {
 			pauseOverlay.update();
@@ -44,15 +68,48 @@ public class Playing extends State implements Statemethods{
 	}
 
 
+	private void checkCloseToBorder() {
+		int playerX = (int) player.getHitbox().x;
+		int diff = playerX - xLvlOffset;
+
+		if (diff > rightBorder)
+			xLvlOffset += diff - rightBorder;
+		else if (diff < leftBorder)
+			xLvlOffset += diff - leftBorder;
+
+		if (xLvlOffset > maxLvlOffsetX)
+			xLvlOffset = maxLvlOffsetX;
+		else if (xLvlOffset < 0)
+			xLvlOffset = 0;
+
+	}
+
+
 	@Override
 	public void draw(Graphics g) {
-		levelManager.draw(g);
-		player.render(g);
+		g.drawImage(backgroundPlayingImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
 		
-		if(paused)
+		drawClouds(g);
+		
+		levelManager.draw(g, xLvlOffset);
+		player.render(g, xLvlOffset);
+		
+		if(paused) {
+			g.setColor(new Color(0, 0, 0, 100));
+			g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
 			pauseOverlay.draw(g);
+		}
 	}
 	
+	private void drawClouds(Graphics g) {
+		for(int i = 0; i < 3; i++)
+			g.drawImage(bigCloud, 0 + i * BIG_CLOUDS_WIDTH - (int) (xLvlOffset *0.3), (int) (10 * Game.SCALE), BIG_CLOUDS_WIDTH, BIG_CLOUDS_HEIGHT, null);
+		
+		for(int i = 0; i < smallCloudPos.length; i++)
+			g.drawImage(smallCloud, SMALL_CLOUDS_WIDTH * 4 * i - (int) (xLvlOffset *0.7), smallCloudPos[i], SMALL_CLOUDS_WIDTH, SMALL_CLOUDS_HEIGHT, null);
+	}
+
+
 	public void unpauseGame() {
 		paused = false;
 	}
